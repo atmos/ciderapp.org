521d8a1d3cccc61734efd2e06f4d7f57ea0337bd
Corey Donohoe <atmos@atmos.org>
2010-06-02 09:51:09 -0700

    symlink out of the correct folder

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
 run "cd #{release_path} && bundle install"
 run "cd #{release_path} && bundle lock"
-run "ln -s #{shared_path}/oauth2.yml #{release_path}/config/"
+run "ln -s #{shared_path}/config/oauth2.yml #{release_path}/config/"
