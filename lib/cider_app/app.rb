fd82a26bbd30e78ac49d7ddfe8a29b8ef9b19762
Corey Donohoe <atmos@atmos.org>
2010-08-31 15:16:42 -0700

    putting solo.rb on the website, removing from the gem

--- lib/cider_app/app.rb
+++ lib/cider_app/app.rb
@@ -23,11 +23,14 @@ module CiderApp
         @recipe_file ||= "cider.tgz"
       end
 
+      def solo_rb
+        @solo_rb ||= File.read(File.dirname(__FILE__) + "/solo.rb.txt")
+      end
+
       def refresh_cookbooks
         Dir.chdir("./tmp") do
           if File.directory?("smeagol")
             Dir.chdir("smeagol") do
-              silently_run("ls -l")
               silently_run("git checkout master")
               silently_run("git reset --hard origin/master")
               silently_run("git pull")
@@ -67,6 +70,11 @@ module CiderApp
     get '/cider.tgz' do
       refresh_cookbooks unless File.exists?("./tmp/#{recipe_file}")
       send_file("./tmp/#{recipe_file}")
+    end
+
+    get '/solo.rb' do
+      content_type 'text/plain', :charset => 'utf-8'
+      solo_rb
     end
 
     get '/latest' do
